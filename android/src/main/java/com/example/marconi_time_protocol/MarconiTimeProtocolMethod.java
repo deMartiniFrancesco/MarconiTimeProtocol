9f4000124a02906f2fe7843dee918c1e0aa1e5fa
FrenkyDema <73547745+deMartiniFrancesco@users.noreply.github.com>
2022-11-16 16:55:49 +0100

    base method class

---
+++ android/src/main/java/com/example/marconi_time_protocol/MarconiTimeProtocolMethod.java
@@ -0,0 +1,12 @@
+package com.example.marconi_time_protocol;
+
+import android.content.Context;
+
+public class MarconiTimeProtocolMethod {
+    private final Context _context;
+
+
+    public MarconiTimeProtocolMethod(Context context) {
+        _context = context;
+    }
+}
--- android/src/main/java/com/example/marconi_time_protocol/MarconiTimeProtocolPlugin.java
+++ android/src/main/java/com/example/marconi_time_protocol/MarconiTimeProtocolPlugin.java
@@ -1,38 +1,64 @@
 package com.example.marconi_time_protocol;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.EventChannel.StreamHandler;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** MarconiTimeProtocolPlugin */
-public class MarconiTimeProtocolPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+/**
+ * MarconiTimeProtocolPlugin
+ */
+public class MarconiTimeProtocolPlugin implements FlutterPlugin, MethodCallHandler, StreamHandler {
+    private static MarconiTimeProtocolMethod marconiTimeProtocolMethod;
+    private MethodChannel methodChannel;
+    private EventChannel eventChannel;
+    private Context context;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "marconi_time_protocol");
-    channel.setMethodCallHandler(this);
-  }
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        context = flutterPluginBinding.getApplicationContext();
+        methodChannel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "marconi_time_protocol");
+        eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "marconi_time_protocol_events");
+        eventChannel.setStreamHandler(this);
+        marconiTimeProtocolMethod =
+                new MarconiTimeProtocolMethod(context);
+        methodChannel.setMethodCallHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "getPlatformVersion" -> result.success("Android " + android.os.Build.VERSION.RELEASE);
+            case "sendPacket" -> {
+
+
+
+
+            }
+            default -> result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onListen(Object arguments, EventChannel.EventSink events) {
+
+    }
+
+    @Override
+    public void onCancel(Object arguments) {
+
+    }
 }
